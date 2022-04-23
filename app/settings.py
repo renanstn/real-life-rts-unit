40f9c17d6c1a6b242c563415e9847aae9938b2ec
Renan Santana <renan_ota@hotmail.com>
2022-04-23 02:17:51 -0300

    add webcam_url env

--- app/settings.py
+++ app/settings.py
@@ -4,3 +4,5 @@ from decouple import config
 MQTT_SERVER_URL = config("MQTT_SERVER_URL")
 MQTT_SERVER_PORT = config("MQTT_SERVER_PORT", cast=int)
 MQTT_TOPIC = config("MQTT_TOPIC")
+
+WEBCAM_URL = config("WEBCAM_URL")
